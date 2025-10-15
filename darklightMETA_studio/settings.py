a725176fb4c6c2aa125259b24d61bc6926c60a95
David Case <dc5spa@hotmail.com>
2025-10-15 19:48:44 +1030

    Master CSS file and updates to settings.py

--- darklightMETA_studio/settings.py
+++ darklightMETA_studio/settings.py
@@ -48,11 +48,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
