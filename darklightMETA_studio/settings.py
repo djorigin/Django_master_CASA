1afb6f640b3ecd87da2866d9b84bb6cfc2a8618a
David Case <dc5spa@hotmail.com>
2025-10-20 20:58:31 +1030

    Update index.html for improved layout and styling; add language selector and quick actions section

--- darklightMETA_studio/settings.py
+++ darklightMETA_studio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.14', 'localhost']
+ALLOWED_HOSTS = ['192.168.0.14', 'localhost', '127.0.0.1']
 
 
 # Application definition
