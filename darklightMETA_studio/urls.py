0b2ced4a58d0450cfc381227c34acc047a1d3831
David Case <dc5spa@hotmail.com>
2025-10-20 20:10:01 +1030

    Add URL pattern for accounts app in urlpatterns

--- darklightMETA_studio/urls.py
+++ darklightMETA_studio/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
